83655686669b6a5d682dc08e9aa9caf12d9bf1dd
Fiantso Harena <fiantsorav@gmail.com>
2024-02-05 20:19:04 +0300

    fix: use dotenv for sensible file

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
-  user: 'postgres',
-  password: 'tsy tadidiko',
-  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: 5432,
-  database: 'userAPI',
+  database: process.env.DATABASE,
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const db = require('./db');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
