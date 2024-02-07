35ff887367c60a99a9ff4630a5f47aff28106e9e
Fiantso Harena <fiantsorav@gmail.com>
2024-02-07 17:51:17 +0300

    chore: configure the root route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/hello', (res, req) => {
   req.send('Hello World');
 });
+app.use('/', express.static(join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
