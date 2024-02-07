ff7507b680b664d8524d729c9542ac97502a37b4
Fiantso Harena <fiantsorav@gmail.com>
2024-02-07 17:40:16 +0300

    chore: create a new endpoint as test

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/hello', (res, req) => {
+  req.send('Hello World');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
