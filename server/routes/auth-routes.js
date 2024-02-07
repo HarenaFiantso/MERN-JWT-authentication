98371e7f6e30c4fbc90476e20767c0254f25c9ff
Fiantso Harena <fiantsorav@gmail.com>
2024-02-07 18:51:18 +0300

    chore: configure .env

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
-    const users = await pool.query('SELECT * FROM users WHERE user_email = ?', [email]);
+    const users = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
 
     if (users.rows.length === 0) {
       return res.status(401).json({ error: 'Email is incorrect' });
